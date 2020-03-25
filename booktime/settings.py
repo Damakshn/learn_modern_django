6b23d7a9be4e506c84958de93dce2574553b67c1
damakshn <damakshon@gmail.com>
2020-03-25 12:26:46 +0800

    Настройка статики, первые шаблоны.

--- booktime/settings.py
+++ booktime/settings.py
@@ -144,7 +144,10 @@ USE_TZ = True
 
 # Static & media
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/media/
+MEDIA_URL = '/media/'
