5ba9b0238ccf41db22fb6c9cab3da2edebd0cc47
Damakshn <damakshon@gmail.com>
2020-04-14 21:32:15 +0800

    Завершен 4 раздел

--- main/urls.py
+++ main/urls.py
@@ -45,4 +45,6 @@ urlpatterns = [
         name="add_to_basket",
     ),
     path("basket/", views.manage_basket, name="basket"),
+    path("order_done/", TemplateView.as_view(template_name="order_done.html"), name="checkout_done"),
+    path("order/address_select/", views.AddressSelectionView.as_view(), name="address_select"),
 ]
